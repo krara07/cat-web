639e93a70a389c1c2d812401073a9675576d95bf
Brytanni <99222703+BrytanniADJ@users.noreply.github.com>
2024-06-04 16:55:28 -0300

    Up

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,25 +1,56 @@
 // MODO CLARO/ESCURO
 let ativar_modos = true;
+let cor_logo = false;
 
-function setCor(){
-    if (ativar_modos){
+function modos(){
+    ativar_modos = !ativar_modos;
+    cor_logo = !cor_logo;
+    if (!ativar_modos){
         document.documentElement.style.setProperty('--base-negro', '#ffffff');
-        document.documentElement.style.setProperty('--griss90', '#f8f8fa');
-        document.documentElement.style.setProperty('--griss80', '#f6f7f8');
-        document.documentElement.style.setProperty('--griss10', '#616b77');
-        document.documentElement.style.setProperty('--griss5', '#1f2328');
+        document.documentElement.style.setProperty('--gris90', '#f8f8fa');
+        document.documentElement.style.setProperty('--gris80', '#f6f7f8'); // modo claro
+        document.documentElement.style.setProperty('--gris10', '#616b77');
+        document.documentElement.style.setProperty('--gris5', '#1f2328');
+        document.documentElement.style.setProperty('--cores', '#ff6b00');
     }
     else{
         document.documentElement.style.setProperty('--base-negro', '#010409');
-        document.documentElement.style.setProperty('--griss90', '#212121');
-        document.documentElement.style.setProperty('--griss80', '#2f2f2f');
-        document.documentElement.style.setProperty('--griss10', '#a0a9b2');
-        document.documentElement.style.setProperty('--griss5', '#ececea');
+        document.documentElement.style.setProperty('--gris90', '#212121');
+        document.documentElement.style.setProperty('--gris80', '#2f2f2f'); // modo escuro
+        document.documentElement.style.setProperty('--gris10', '#a0a9b2');
+        document.documentElement.style.setProperty('--gris5', '#ececea');
+        document.documentElement.style.setProperty('--cores', '#0094ff')
     }
 }
-function modos() {
-    ativar_modos = !ativar_modos;
-    setCor();
-}
 
-setCor();   
+
+document.addEventListener('DOMContentLoaded', (event) => {
+    const imagem = document.getElementById('logo');
+    logo.addEventListener('mouseenter', () => {
+        if (!cor_logo){
+            imagem.src = 'assets/images/logo-cl.png';
+        }else{
+            imagem.src = 'assets/images/logo-es.png';
+        }
+    });
+    imagem.addEventListener('mouseleave', () =>{
+        if (!cor_logo){
+            imagem.src = 'assets/images/logo-es.png';
+        }else{
+            imagem.src = 'assets/images/logo-cl.png';
+        }
+    });
+});
+
+
+window.addEventListener('scroll', function(){
+    var header = document.getElementById('header');
+    var div = document.getElementById('div');
+    if (window.scrollY > 10){
+        header.classList.add('scrolled');
+        div.classList.add('add');
+    }else{
+        header.classList.remove('scrolled');
+        div.classList.add('none');
+    }
+});
