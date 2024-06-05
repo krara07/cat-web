b1f72da0e17fb7f7dda04bdf9bf28d12de17c2b3
Brytanni <99222703+BrytanniADJ@users.noreply.github.com>
2024-06-05 16:54:11 -0300

    Up
    Fazendo tela de login

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,24 +2,42 @@
 let ativar_modos = true;
 let cor_logo = false;
 
+login = false;
+
+function login(){
+    var login_nome = document.getElementById('login');
+    if(login == false){
+        login_nome.classList.add('login'); // classe por padrão
+    }else{
+        login_nome.classList.add('logado'); // ativar a classe após a confirmação de login
+    }
+}
+
 function modos(){
     ativar_modos = !ativar_modos;
     cor_logo = !cor_logo;
+    const login = document.getElementById('login');
     if (!ativar_modos){
         document.documentElement.style.setProperty('--base-negro', '#ffffff');
         document.documentElement.style.setProperty('--gris90', '#f8f8fa');
         document.documentElement.style.setProperty('--gris80', '#f6f7f8'); // modo claro
+        // document.documentElement.style.setProperty('--gris70', '#616b77');
+        // document.documentElement.style.setProperty('--gris60', '#616b77');
         document.documentElement.style.setProperty('--gris10', '#616b77');
         document.documentElement.style.setProperty('--gris5', '#1f2328');
         document.documentElement.style.setProperty('--cores', '#ff6b00');
+        login.src = 'assets/images/login-cl.png';
     }
     else{
         document.documentElement.style.setProperty('--base-negro', '#010409');
         document.documentElement.style.setProperty('--gris90', '#212121');
-        document.documentElement.style.setProperty('--gris80', '#2f2f2f'); // modo escuro
+        document.documentElement.style.setProperty('--gris80', '#242424'); // modo escuro
+        document.documentElement.style.setProperty('--gris70', '#2b2b2b');
+        document.documentElement.style.setProperty('--gris60', '#383838');
         document.documentElement.style.setProperty('--gris10', '#a0a9b2');
         document.documentElement.style.setProperty('--gris5', '#ececea');
-        document.documentElement.style.setProperty('--cores', '#0094ff')
+        document.documentElement.style.setProperty('--cores', '#0094ff');
+        login.src = 'assets/images/login-es.png';
     }
 }
 
@@ -36,6 +54,7 @@ document.addEventListener('DOMContentLoaded', (event) => {
     imagem.addEventListener('mouseleave', () =>{
         if (!cor_logo){
             imagem.src = 'assets/images/logo-es.png';
+            
         }else{
             imagem.src = 'assets/images/logo-cl.png';
         }
