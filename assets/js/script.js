8df2582e338536570e664d7a4a1efe307a7196aa
Brytanni <99222703+BrytanniADJ@users.noreply.github.com>
2024-06-28 16:57:07 -0300

    up

--- assets/js/script.js
+++ assets/js/script.js
@@ -58,24 +58,26 @@ function modos(){
 
 // trocar imagens
 document.addEventListener('DOMContentLoaded', (event) => {
-    const imagem = document.getElementById('logo');
-    logo.addEventListener('mouseenter', () => {
-        if (!cor_logo){
-            imagem.src = 'assets/images/logo-cl.png';
-        }else{
-            imagem.src = 'assets/images/logo-es.png';
-        }
-    });
-    imagem.addEventListener('mouseleave', () =>{
-        if (!cor_logo){
-            imagem.src = 'assets/images/logo-es.png';
-            
-        }else{
-            imagem.src = 'assets/images/logo-cl.png';
-        }
+    const imagens = document.querySelectorAll('.img-logo'); // Seleciona todos os elementos com a classe 'logo'
+    
+    imagens.forEach(imagem => {
+        imagem.addEventListener('mouseenter', () => {
+            if (!cor_logo){
+                imagem.src = 'assets/images/logo-cl.png';
+            } else {
+                imagem.src = 'assets/images/logo-es.png';
+            }
+        });
+
+        imagem.addEventListener('mouseleave', () => {
+            if (!cor_logo){
+                imagem.src = 'assets/images/logo-es.png';
+            } else {
+                imagem.src = 'assets/images/logo-cl.png';
+            }
+        });
     });
 });
-
 
 // header fixa, transparente
 window.addEventListener('scroll', function(){
@@ -85,110 +87,25 @@ window.addEventListener('scroll', function(){
         header.classList.add('scrolled');
         div.classList.remove('none');
         div.classList.add('add');
-    } else {
+    }else{
         header.classList.remove('scrolled');
         div.classList.remove('add');
         div.classList.add('none');
     }
 });
 
+//checkbox marcado para ver senha
+document.addEventListener('DOMContentLoaded', (event) => {
+    const check = document.getElementById('ver_senha');
+    const senha = document.getElementById('password');
 
-// password -> text - Não funciona
-function ver_senha(){
-    var senha1 = document.getElementById("pass");
-    var senha2 = document.getElementById("pass2");
-    if (senha1.type === "password") {
-        senha1.type = "text";
-        senha2.type = "text";
-    } else {
-        senha1.type = "password";
-        senha2.type = "password";
-    }
-}
-
-// atualizar imagem de perfil => login.html
-function upload_img(){
-    var fileInput = document.getElementById('imageUpload');
-    var previewImg = document.getElementById('previewImg');
-    
-    if (fileInput.files && fileInput.files[0]) {
-        var reader = new FileReader();
-        
-        reader.onload = function(e) {
-            var imageUrl = e.target.result;
-            previewImg.src = imageUrl;
-            localStorage.setItem('uploadedImage', imageUrl);
+    check.addEventListener('change', () => {
+        if(check.checked){
+            // checkbox marcado
+            senha.type = 'text';
+        }else{
+            // checkbox não marcado
+            senha.type = 'password';
         }
-        
-        reader.readAsDataURL(fileInput.files[0]);
-    }
-}
-
-document.addEventListener('DOMContentLoaded', function(){
-    var previewImg = document.getElementById('previewImg');
-    var storedImage = localStorage.getItem('uploadedImage');
-
-    if (storedImage) {
-        previewImg.src = storedImage;
-    }
+    });
 });
-
-function novo_user(){
-    var nome = document.getElementById("nome"); //input
-    var erro_nome = document.getElementById("erro_nome"); //span
-
-    var cod = document.getElementById("cod"); //input
-    var erro_cod = document.getElementById("erro_cod"); //span
-
-    var email = document.getElementById("email"); //input
-    var erro_email = document.getElementById("erro_email"); //span
-
-    var pass = document.getElementById("pass"); //input
-    var erro_pass = document.getElementById("erro_pass"); //span
-
-    var pass2 = document.getElementById("pass2"); //input
-    var erro_pass2 = document.getElementById("erro_pass2"); //span
-
-    if(!nome.value){
-        erro_nome.textContent = "*";
-        erro_nome.style.display = "inline";
-    }
-    
-    if(!cod.value){
-        erro_cod.textContent = "*";
-        erro_cod.style.display = "inline";
-    }
-    
-    if(!pass.value || !pass2.value){
-        erro_pass.textContent = "*";
-        erro_pass.style.display = "inline";
-        erro_pass2.textContent = "*";
-        erro_pass2.style.display = "inline";
-    }
-
-    if(!email.value){
-        erro_email.textContent = "*";
-        erro_email.style.display = "inline";
-    }
-}
-
-function user_apro(){
-    var cod = document.getElementById("cod"); //input
-    var erro_cod = document.getElementById("erro_cod"); //span
-
-    var pass = document.getElementById("pass"); //input
-    var erro_pass = document.getElementById("erro_pass"); //span
-
-    if(!cod.value){
-        erro_cod.textContent = "*";
-        erro_cod.style.display = "inline";
-    }
-
-    if(!pass.value){
-        erro_pass.textContent = "*";
-        erro_pass.style.display = "inline";
-    }
-}
-
-
-
