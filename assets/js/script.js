423e97dea995ceae94d6c3551841cf26f13b17b6
Brytanni <99222703+BrytanniADJ@users.noreply.github.com>
2024-07-08 16:54:49 -0300

    up

--- assets/js/script.js
+++ assets/js/script.js
@@ -115,48 +115,118 @@ document.addEventListener('DOMContentLoaded', (event) => {
 });
 
 // cat-inicio
-
 document.getElementById("tipoCat").addEventListener('change', function(){
     const tipoCat = this.value;
 
     const inicial = document.querySelectorAll(".inicial-cat");
     const reaber = document.querySelectorAll(".reabertura-cat");
     const obito = document.querySelectorAll(".obito-cat");
+    const cat_part2 = document.getElementById("cat-part2");
 
-    // reinicia deixando tudo visivel quando mudado
-    inicial.forEach(function(inicial){
-        inicial.style.display = "none";
-    });
-    reaber.forEach(function(reaber){
-        reaber.style.display = "none";
-    });
-    obito.forEach(function(obito){
-        obito.style.display = "none";
-    });
+    // reinicia deixando tudo invisivel quando mudado
+    inicial.forEach(function(inicial){inicial.style.display = "none";});
 
-    if(tipoCat == "0"){
-        alert("Uma opção precisa ser selecionada")
-    }else if(tipoCat == "1"){
+    reaber.forEach(function(reaber){reaber.style.display = "none";});
+
+    obito.forEach(function(obito){obito.style.display = "none";});
+
+    if(tipoCat == 1){
         inicial.forEach(function(inicial){
             inicial.style.display = "block";
         });
-    }else if(tipoCat == "2"){
+    }else if(tipoCat == 2){
         reaber.forEach(function(reaber){
             reaber.style.display = "block";
         });
-    }else{
+    }else if(tipoCat == 3){
         obito.forEach(function(obito){
             obito.style.display = "block";
         });
+    }else{
+        alert("Uma opção precisa ser selecionada")
     }
+    cat_part2.style.visibility = "visible";
+
+    $(document).ready(function(){
+        $(".cpf-mask").inputmask("999.999.999-99");
+    });
 });
 
+//cat inicio
 document.getElementById("tipo_empre-cat").addEventListener('change', function(){
     const tipo_empre = this.value;
 
     const cnpj = document.querySelectorAll(".cnpj-cat");
     const cei = document.querySelectorAll(".cei-cat");
     const cpf = document.querySelectorAll(".cpf-cat");
+    const nit = document.querySelectorAll(".nit-cat");
+    const caepf = document.querySelectorAll(".caepf-cat");
+    const cno = document.querySelectorAll(".cno-cat");
+
+    //reinicia deixado tudo invisível quando mudado
+
+    cnpj.forEach(function(cnpj){cnpj.style.display = "none";});
+
+    cei.forEach(function(cei){cei.style.display = "none";});
+
+    cpf.forEach(function(cpf){cpf.style.display = "none";});
+
+    nit.forEach(function(nit){nit.style.display = "none";});
+
+    caepf.forEach(function(caepf){caepf.style.display = "none";});
+
+    cno.forEach(function(cno){cno.style.display = "none";});
+
+    if(tipo_empre == 1){
+        cnpj.forEach(function(cnpj){
+            cnpj.style.display = "block";
+        });
+        $(document).ready(function(){
+            $(".cnpj-mask").inputmask("99.999.999/9999-99"); /* Inputmask */
+        });
+    }else if(tipo_empre == 2){
+        cei.forEach(function(cei){
+            cei.style.display = "block";
+        });
+        $(document).ready(function(){
+            $(".cei-mask").inputmask("99.999.99999/99");
+        });
+    }else if(tipo_empre == 3){
+        cpf.forEach(function(cpf){
+            cpf.style.display = "block";
+        });
+        $(document).ready(function(){
+            $(".cpf-mask").inputmask("999.999.999-99");
+        });
+    }else if(tipo_empre == 4){
+        nit.forEach(function(nit){
+            nit.style.display = "block";
+        });
+        $(document).ready(function(){
+            $(".nit-mask").inputmask("999.99999.99-9");
+        });
+    }else if(tipo_empre == 5){
+        caepf.forEach(function(caepf){
+            caepf.style.display = "block";
+        });
+        $(document).ready(function(){
+            $(".caepf-mask").inputmask("999.999.999/999-99");
+        });
+    }else if(tipo_empre == 6){
+        cno.forEach(function(cno){
+            cno.style.display = "block";
+        });
+        $(document).ready(function(){
+            $(".cno-mask").inputmask("99.999.999999/99");
+        });
+    }else{
+        alert("Uma opção precisa ser selecionada")
+    }
 });
+
+//inputmask
+
+
+
 //print da tela do documento
 // window.print()
